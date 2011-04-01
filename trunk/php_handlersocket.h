b5329451a5df613b4f15073952acf64683da9529
avuenta <avuenta@4c154c70-137a-cad6-8d9a-0cdd73844609>
2011-04-01 08:21:41 +0000

    Fixed port parameter.

--- trunk/php_handlersocket.h
+++ trunk/php_handlersocket.h
@@ -2,7 +2,7 @@
 #ifndef PHP_HANDLERSOCKET_H
 #define PHP_HANDLERSOCKET_H
 
-#define HANDLERSOCKET_EXTENSION_VERSION "0.0.7"
+#define HANDLERSOCKET_EXTENSION_VERSION "0.0.8"
 
 extern zend_module_entry handlersocket_module_entry;
 #define phpext_handlersocket_ptr &handlersocket_module_entry
